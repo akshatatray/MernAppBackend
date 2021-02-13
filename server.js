2c15dbc235e1c0f6617b5dfb4d3a45b10854a05e
Akshat Atray <64903530+akshatatray@users.noreply.github.com>
2021-02-13 10:47:49 +0530

    Fixing

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
+const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
-const stripe = require('stripe')('sk_test_51H0PBdDthXXzKqsl0VeeWWGtycmhb2LFx7f5MddNGLnFAG7qua2oxqTVzkxHcA6SnGzg2TOy4tXxEcfUFFAYM2R000oCUoCbDL');
-const { MONGODBURI } = require('./keys');
+const stripe = require("stripe")(
+  "sk_test_51H0PBdDthXXzKqsl0VeeWWGtycmhb2LFx7f5MddNGLnFAG7qua2oxqTVzkxHcA6SnGzg2TOy4tXxEcfUFFAYM2R000oCUoCbDL"
+);
+const { MONGODBURI } = require("./keys");
 
 app.use(cors());
 app.use(express.json());
-app.use(require('./Routes/ItemRoutes'));
-app.use(require('./Routes/ShopRoutes')); 
+app.use(require("./Routes/ItemRoutes"));
+app.use(require("./Routes/ShopRoutes"));
 
-app.use(require('./Routes/PaymentRoutes'));
-app.use("/",require("./Routes/userRouter"));
+app.use(require("./Routes/PaymentRoute"));
+app.use("/", require("./Routes/userRouter"));
 
-mongoose.connect(MONGODBURI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+mongoose.connect(MONGODBURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 const connection = mongoose.connection;
-connection.once('open', () => {
-     console.log('MONGO DB Connected Successfully!');
+connection.once("open", () => {
+  console.log("MONGO DB Connected Successfully!");
 });
 
-
 app.listen(PORT, () => {
-     console.log(`Server Stated at ${ PORT }`);
+  console.log(`Server Stated at ${PORT}`);
 });
